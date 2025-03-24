5ccf9f39483b73906c039fb36695c2de01fd649c
Jacob Banks <jacob.banks@caa.com>
2025-03-23 18:29:30 -0700

    hello world refactor

--- hello_world/hello_test.go
+++ hello_world/hello_test.go
@@ -3,9 +3,22 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Banks")
-	want := "Hello, Banks"
+	t.Run("saying Hello to the perople", func(t *testing.T) {
+		got := Hello("Banks")
+		want := "Hello, Banks"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("default to 'Hello, World'", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q, want %q", got, want)
 	}
+	
 }
