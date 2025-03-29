bef5db20454414dc3f5b5a8167d230e171f55c06
Jacob Banks <jacob.banks@caa.com>
2025-03-29 14:32:46 -0700

    slices and arrays

--- arrays_and_slices/sum.go
+++ arrays_and_slices/sum.go
@@ -1,16 +1,31 @@
 package arrays_and_slices
 
-
-import (
-	"fmt"
-)
-
-
 func AddNums(nums []int) int {
-	fmt.Printf("input %d", nums)
 	sum := 0
 	for _, num := range nums {
 		sum += num
 	}
 	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var res []int
+	for _, num := range numbersToSum {
+		res = append(res, AddNums(num))
+	}
+	return res
+
+}
+
+func SumAllTails(nums ...[]int) []int {
+	var res []int
+	for _, num := range nums {
+		if len(num) == 0 {
+			res = append(res, 0)
+		} else {
+			tail := num[1:]
+			res = append(res, AddNums(tail))
+		}
+	}
+	return res
 }
--- arrays_and_slices/sum_test.go
+++ arrays_and_slices/sum_test.go
@@ -1,16 +1,46 @@
 package arrays_and_slices
 
 import (
+	"reflect"
 	"testing"
 )
 
 func TestSum(t *testing.T) {
-	t.Run("Add collection of any size", func(t *testing.T) {
-		nums := []int{1,2,3}
-		got := AddNums(nums)
-		want := 6
-		if got != want {
-			t.Fatalf("got %d wanted %d, given %v", got, want, nums)
-		}
+	nums := []int{1, 2, 3}
+	got := AddNums(nums)
+	want := 6
+	if got != want {
+		t.Fatalf("got %d wanted %d, given %v", got, want, nums)
+	}
+}
+
+func TestSumAll(t *testing.T) {
+	numOne := []int{1, 2, 3}
+	numTwo := []int{3, 2, 1}
+
+	got := SumAll(numOne, numTwo)
+	want := []int{6, 6}
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("got %v wanted %v", got, want)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	assertSums := func(t testing.TB, got, want []int) {
+		t.Helper()
+		if !reflect.DeepEqual(got, want) {
+			t.Fatalf("got %v wanted %v", got, want)
+		}	
+	}
+	t.Run("Add Tails", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3}, []int{4, 5, 6})
+		want := []int{5, 11}
+		assertSums(t, got, want)
+	})
+
+	t.Run("Add Tails, allowing for empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{4, 5, 6})
+		want := []int{0, 11}
+		assertSums(t, got, want)
 	})
 }
